7ec83533f07909a39ded374104cd201da216779e
Brecht Vanhaesebrouck <brecht.vanhaesebrouck@sea-invest.com>
2023-01-26 17:16:12 +0100

    unimportant stuff

---
+++ eLime.NetDaemonApps/apps/Guard/Guard.cs
@@ -0,0 +1,27 @@
+namespace eLime.NetDaemonApps.apps.Guard;
+
+
+//[NetDaemonApp(Id = "guard")]
+//public class Guard : IAsyncInitializable, IAsyncDisposable
+//{
+//    private readonly IHomeAssistantRunner _runner;
+
+//    public Guard(IHomeAssistantRunner runner)
+//    {
+//        _runner = runner;
+//    }
+
+//    public Task InitializeAsync(CancellationToken cancellationToken)
+//    {
+//        _runner.OnDisconnect.Subscribe(x => throw new Exception("Forcing container crash after home assistant connection got lost. Otherwise CPU somehow explodes after 4 hours"));
+
+//        return Task.CompletedTask;
+//    }
+
+
+
+//    public ValueTask DisposeAsync()
+//    {
+//        return ValueTask.CompletedTask;
+//    }
+//}
