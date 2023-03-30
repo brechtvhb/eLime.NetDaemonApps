8ddfa649e2c89a0e2e62a150e123d4ee48a5de31
Brecht Vanhaesebrouck <brecht.vanhaesebrouck@sea-invest.com>
2023-03-30 22:24:03 +0200

    Couldn't resist, one more test

--- eLime.NetDaemonApps.Tests/SmartWateringTests.cs
+++ eLime.NetDaemonApps.Tests/SmartWateringTests.cs
@@ -63,7 +63,7 @@ public class SmartWateringTests
     }
 
     [TestMethod]
-    public void Below_Low_Volume_Triggers_Valve()
+    public void Below_Low_Volume_Triggers_Valve_On()
     {
         // Arrange
         var zone = new ContainerIrrigationZoneBuilder(_testCtx)
@@ -110,5 +110,31 @@ public class SmartWateringTests
         //Assert
         Assert.AreEqual(NeedsWatering.Ongoing, irrigation.Zones.First().Zone.State);
         Assert.AreEqual(_testCtx.Scheduler.Now, irrigation.Zones.First().Zone.WateringStartedAt);
+    }
+
+    [TestMethod]
+    public void Overflow_On_Triggers_Valve_Off()
+    {
+        // Arrange
+        var zone = new ContainerIrrigationZoneBuilder(_testCtx)
+            .Build();
+
+        var irrigation = new SmartIrrigationBuilder(_testCtx, _logger, _mqttEntityManager, _testCtx.Scheduler)
+            .With(_pumpSocket, 2000)
+            .With(_availableRainWaterSensor, 2000)
+            .AddZone(zone)
+            .Build();
+
+        zone.SetMode(ZoneMode.Automatic);
+        _testCtx.TriggerStateChange(_testCtx.HaContext.Entity("sensor.pond_volume"), "5500");
+        _testCtx.TriggerStateChange(_testCtx.HaContext.Entity("binary_sensor.pond_overflow"), "off");
+        _testCtx.TriggerStateChange(_testCtx.HaContext.Entity("switch.pond_valve"), "on");
+
+        //Act
+        _testCtx.TriggerStateChange(_testCtx.HaContext.Entity("binary_sensor.pond_overflow"), "on");
+
+        //Assert
+        Assert.AreEqual(NeedsWatering.No, irrigation.Zones.First().Zone.State);
+        _testCtx.VerifySwitchTurnOff(new BinarySwitch(_testCtx.HaContext, "switch.pond_valve"), Moq.Times.Once);
     }
 }
