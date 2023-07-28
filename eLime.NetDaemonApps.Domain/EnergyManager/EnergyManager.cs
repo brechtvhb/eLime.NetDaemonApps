653ea4bfa40fd6f0aa466fd20a6c9723ab9fce47
Brecht Vanhaesebrouck <brecht.vanhaesebrouck@sea-invest.com>
2023-07-28 13:32:34 +0200

    x

--- eLime.NetDaemonApps.Domain/EnergyManager/EnergyManager.cs
+++ eLime.NetDaemonApps.Domain/EnergyManager/EnergyManager.cs
@@ -124,7 +124,7 @@ public class EnergyManager : IDisposable
             if (netChange == 0)
                 continue;
 
-            _logger.LogDebug("{Consumer}: Changed current for dynamic consumer, to  {DynamicCurrent} A.", dynamicLoadConsumer.Name, current);
+            _logger.LogDebug("{Consumer}: Changed current for dynamic consumer, to  {DynamicCurrent} A (Net change: {NetLoadChange} W).", dynamicLoadConsumer.Name, current, netChange);
             estimatedLoad += netChange;
         }
 
