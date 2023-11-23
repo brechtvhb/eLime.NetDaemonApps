0c08f79895ded11cf4e39655779032d7fbcbe8e2
Brecht Vanhaesebrouck <abrecht.vanhaesebrouck@sea-invest.com>
2023-11-23 16:49:22 +0100

    The usual story

--- eLime.NetDaemonApps.Domain/FlexiScenes/Rooms/Room.cs
+++ eLime.NetDaemonApps.Domain/FlexiScenes/Rooms/Room.cs
@@ -153,7 +153,7 @@ public class Room : IAsyncDisposable
         _fileStorage = fileStorage;
 
         Name = config.Name;
-        EnsureEnabledSwitchExists().RunSync();
+        EnsureSensorsExist().RunSync();
 
         if (autoTransitionDebounce != TimeSpan.Zero)
             AutoTransitionDebounceDispatcher = new(autoTransitionDebounce);
@@ -282,22 +282,22 @@ public class Room : IAsyncDisposable
         GuardTask = _scheduler.RunEvery(TimeSpan.FromSeconds(5), _scheduler.Now, CheckForMotion);
     }
 
-    private async Task EnsureEnabledSwitchExists()
+    private async Task EnsureSensorsExist()
     {
         var baseName = $"sensor.flexilights_{Name.MakeHaFriendly()}";
         var switchName = $"switch.flexilights_{Name.MakeHaFriendly()}";
 
-        if (_haContext.Entity(switchName).State == null)
-        {
-            _logger.LogDebug("Creating Entities for room '{room}' in home assistant.", Name);
-            var enabledSwitchOptions = new EnabledSwitchAttributes { Icon = "fapro:palette", Device = GetDevice() };
-            await _mqttEntityManager.CreateAsync(switchName, new EntityCreationOptions(Name: $"Flexi lights - {Name}", DeviceClass: "switch", Persist: true), enabledSwitchOptions);
-            Enabled = true;
-            await _mqttEntityManager.SetStateAsync(switchName, "ON");
+        //if (_haContext.Entity(switchName).State == null)
+        //{
+        _logger.LogDebug("Creating Entities for room '{room}' in home assistant.", Name);
+        var enabledSwitchOptions = new EnabledSwitchAttributes { Icon = "fapro:palette", Device = GetDevice() };
+        await _mqttEntityManager.CreateAsync(switchName, new EntityCreationOptions(Name: $"Flexi lights - {Name}", DeviceClass: "switch", Persist: true), enabledSwitchOptions);
+        Enabled = true;
+        await _mqttEntityManager.SetStateAsync(switchName, "ON");
 
-            var initiatedByOptions = new EnumSensorOptions { Icon = "hue:motion-sensor-movement", Device = GetDevice(), Options = Enum<InitiatedBy>.AllValuesAsStringList() };
-            await _mqttEntityManager.CreateAsync($"{baseName}_initiated_by", new EntityCreationOptions(UniqueId: $"{baseName}_initiated_by", Name: $"Flex scene {Name} - Initiated by", DeviceClass: "enum", Persist: true), initiatedByOptions);
-        }
+        var initiatedByOptions = new EnumSensorOptions { Icon = "hue:motion-sensor-movement", Device = GetDevice(), Options = Enum<InitiatedBy>.AllValuesAsStringList() };
+        await _mqttEntityManager.CreateAsync($"{baseName}_initiated_by", new EntityCreationOptions(UniqueId: $"{baseName}_initiated_by", Name: $"Flex scene {Name} - Initiated by", DeviceClass: "enum", Persist: true), initiatedByOptions);
+        //}
 
 
         var observer = await _mqttEntityManager.PrepareCommandSubscriptionAsync(switchName);
@@ -350,7 +350,7 @@ public class Room : IAsyncDisposable
             }
         }
 
-        _logger.LogDebug("Retrieved flexilight state from Home assistant for room '{room}'.", Name);
+        _logger.LogDebug("Retrieved flexilight state from file storage for room '{room}'.", Name);
 
         await ScheduleTurnOffAt();
         await ScheduleClearIgnorePresence();
