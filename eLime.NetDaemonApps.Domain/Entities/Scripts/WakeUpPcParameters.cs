ec7b369b8f47dc1950a836fd2a6633090456c614
Brecht Vanhaesebrouck <abrecht.vanhaesebrouck@sea-invest.com>
2023-12-29 21:23:41 +0100

    Add broadcast address to wake up pc script

--- eLime.NetDaemonApps.Domain/Entities/Scripts/WakeUpPcParameters.cs
+++ eLime.NetDaemonApps.Domain/Entities/Scripts/WakeUpPcParameters.cs
@@ -6,4 +6,6 @@ public class WakeUpPcParameters
 {
     [JsonPropertyName("mac_address")]
     public string MacAddress { get; set; }
+    [JsonPropertyName("broadcast_address")]
+    public string BroadcastAddress { get; set; }
 }
