7d0e1038aed71eccb9677bf6790104d619dd0966
Brecht Vanhaesebrouck <abrecht.vanhaesebrouck@sea-invest.com>
2024-01-10 10:43:51 +0100

    Update state in HA after re-init

--- eLime.NetDaemonApps.Domain/SmartIrrigation/SmartIrrigation.cs
+++ eLime.NetDaemonApps.Domain/SmartIrrigation/SmartIrrigation.cs
@@ -95,6 +95,8 @@ public class SmartIrrigation : IDisposable
             DebounceStartWatering();
             DebounceStopWatering();
         });
+
+        UpdateStateInHomeAssistant().RunSync();
     }
 
 
