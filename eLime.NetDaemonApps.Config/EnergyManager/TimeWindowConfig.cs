a8add5eb195a9e33f69ea6b2e835391f203627b6
Brecht Vanhaesebrouck <abrecht.vanhaesebrouck@sea-invest.com>
2024-01-16 22:40:51 +0100

    mo hooooooow

--- eLime.NetDaemonApps.Config/EnergyManager/TimeWindowConfig.cs
+++ eLime.NetDaemonApps.Config/EnergyManager/TimeWindowConfig.cs
@@ -3,6 +3,6 @@
 public class TimeWindowConfig
 {
     public string ActiveEntity { get; set; }
-    public TimeSpan Start { get; }
-    public TimeSpan End { get; }
+    public TimeSpan Start { get; set; }
+    public TimeSpan End { get; set; }
 }
