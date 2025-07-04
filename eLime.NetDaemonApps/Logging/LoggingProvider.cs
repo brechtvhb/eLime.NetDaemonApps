6cb4fd0fc59acab5734e8839508289db537cfe91
Brecht Vanhaesebrouck <abrecht.vanhaesebrouck@sea-invest.com>
2025-07-04 09:24:45 +0200

    More optimizations

--- eLime.NetDaemonApps/Logging/LoggingProvider.cs
+++ eLime.NetDaemonApps/Logging/LoggingProvider.cs
@@ -1,28 +1,48 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
-namespace eLime.NetDaemonApps.Logging
+namespace eLime.NetDaemonApps.Logging;
+
+public static class CustomLoggingProvider
 {
-    public static class CustomLoggingProvider
+    /// <summary>
+    ///     Adds standard Serilog logging configuration, from appsettings, as per:
+    ///     https://github.com/datalust/dotnet6-serilog-example
+    /// </summary>
+    /// <param name="builder"></param>
+    public static IHostBuilder UseCustomLogging(this IHostBuilder builder)
     {
-        /// <summary>
-        ///     Adds standard Serilog logging configuration, from appsettings, as per:
-        ///     https://github.com/datalust/dotnet6-serilog-example
-        /// </summary>
-        /// <param name="builder"></param>
-        public static IHostBuilder UseCustomLogging(this IHostBuilder builder)
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile("appsettings.development.json", optional: true)
-                .Build();
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.development.json", optional: true)
+            .Build();
 
-            var logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
+        var logger = new LoggerConfiguration()
+            .Enrich.With(new ClassNameEnricher())
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
 
-            return builder.UseSerilog(logger);
-        }
+        return builder.UseSerilog(logger);
+    }
+}
+
+public class ClassNameEnricher : ILogEventEnricher
+{
+
+    public ClassNameEnricher()
+    {
+
+    }
+
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        logEvent.Properties.TryGetValue("SourceContext", out var sourceContext);
+        var sourceContextAsString = sourceContext?.ToString();
+
+        var property = propertyFactory.CreateProperty("ClassName", sourceContextAsString?[(sourceContextAsString.LastIndexOf('.') + 1)..]?.Replace("\"", "") ?? sourceContextAsString);
+        logEvent.AddPropertyIfAbsent(property);
     }
 }
