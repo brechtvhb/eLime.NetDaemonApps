ca190981a1bd35743ff3ef5cee55aaf150e128a7
Brecht Vanhaesebrouck <abrecht.vanhaesebrouck@sea-invest.com>
2025-10-25 12:56:36 +0200

    Tweak

--- eLime.NetDaemonApps.Domain/SmartHeatPump/SmartHeatPump.cs
+++ eLime.NetDaemonApps.Domain/SmartHeatPump/SmartHeatPump.cs
@@ -88,11 +88,13 @@ public class SmartHeatPump : IDisposable
     {
         try
         {
-            if (e.Sensor.State != 0)
+            var recalculateEnergyDemand = e.New?.State == 0 && e.Old?.State != null && e.Old.State != 0 || e.New?.State != 0 && e.Old?.State is 0;
+            if (!recalculateEnergyDemand)
                 return;
 
             await ResolveRoomEnergyDemand(HomeAssistant.RoomTemperatureSensor.State);
             await ResolveHotWaterEnergyDemand(HomeAssistant.HotWaterTemperatureSensor.State);
+
         }
         catch (Exception ex)
         {
